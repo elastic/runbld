6a29a5e4fc90fcb8f3dfe7d49543d45be0c277c6
Drew Raines <drew@raines.me>
2015-11-03 07:52:24 -0600

    Break out project org, name, branch

--- src/runbld/build.clj
+++ src/runbld/build.clj
@@ -9,6 +9,19 @@
           (date/yyyymmdd-hhmmss)
           (make-rand-uuid)))
 
+(defn split-jenkins-name [s]
+  (let [[_ org project branch] (re-find #"^([^,]+),([^,]+),([^,]+)$" s)]
+    {:org org
+     :project project
+     :branch branch}))
+
+(defn inherited-build-info [opts]
+  (if (get-in opts [:env "JENKINS_HOME"])
+    (split-jenkins-name (get-in opts [:env "JOB_NAME"]))))
+
 (defn wrap-build-meta [proc]
   (fn [opts]
-    (proc (assoc opts :id (make-id)))))
+    (proc (assoc opts
+                 :build (merge
+                         {:id (make-id)}
+                         (inherited-build-info opts))))))
