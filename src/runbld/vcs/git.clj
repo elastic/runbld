b377178db3abb0d0a49614008475f0b835879a4b
Drew Raines <drew@raines.me>
2015-11-09 11:51:20 -0600

    Add git workflow test

--- src/runbld/vcs/git.clj
+++ src/runbld/vcs/git.clj
@@ -1,10 +1,13 @@
 (ns runbld.vcs.git
   (:require [clj-jgit.porcelain :as git]
             [clojure.java.io :as jio]
-            [runbld.util.io :as io]))
+            [environ.core :as environ]
+            [runbld.util.date :as date]
+            [runbld.util.io :as io]
+            [slingshot.slingshot :refer [throw+]]))
 
 (defn init-test-repo [dir]
-  (let [basename "build.sh"
+  (let [basename "this-is-a-test-repo.sh"
         f (jio/file dir basename)
         repo (git/git-init dir)]
     (spit f "echo building... done")
@@ -26,15 +29,42 @@
     (catch java.io.FileNotFoundException _)))
 
 (defn fetch [dir]
-  (git/git-fetch-all (git/load-repo dir)))
+  (try
+    (git/git-fetch-all (git/load-repo dir))
+    (catch org.eclipse.jgit.api.errors.InvalidRemoteException e
+      (if (environ/env :dev)
+        (println "warning: can't fetch origin for" dir)
+        (throw+ {:error ::invalid-origin
+                 :msg (format "trying to fetch %s" dir)
+                 :exception e})))))
 
 (defn clone [remote parent dest]
   (io/mkdir-p parent)
-  (git/git-clone remote dest))
+  (git/git-clone2 remote {:path dest :bare false}))
 
 (defn clone-or-fetch [basedir remote org project]
-  (let [orgdir (jio/file basedir org)
-        gitdir (jio/file orgdir project)]
+  (let [orgdir (str (jio/file basedir org))
+        gitdir (str (jio/file orgdir project))]
     (if (try-repo gitdir)
       (fetch gitdir)
-      (clone remote orgdir gitdir))))
+      (clone remote orgdir gitdir))
+    gitdir))
+
+(defn commit-map [commit]
+  (let [author (.getAuthorIdent commit)]
+    {:commit (.getName commit)
+     :commit-time (and author (date/date-to-iso
+                               (.getWhen author)))
+     :author-name (and author (.getName author))
+     :author-email (and author (.getEmailAddress author))}))
+
+(defn checkout-workspace [clone-home remote workspace org project branch]
+  (io/rmdir-rf workspace)
+  (let [cached-remote (clone-or-fetch clone-home remote org project)
+        absremote (.getCanonicalPath (jio/file cached-remote))
+        clonecmd (git/git-clone2 absremote {:path workspace :bare false
+                                     :clone-all-branches true})
+        workspace-repo (git/load-repo workspace)
+        workspace-ref (git/git-checkout workspace-repo branch false true)
+        HEAD (first (git/git-log workspace-repo))]
+    (commit-map HEAD)))
--- test/runbld/test/build_test.clj
+++ test/runbld/test/build_test.clj
@@ -1,7 +1,10 @@
 (ns runbld.test.build-test
-  (:require [clojure.test :refer :all])
+  (:require [clojure.test :refer :all]
+            [clojure.java.io :as jio]
+            [clj-jgit.porcelain :as jgit])
   (:require [runbld.build :as build]
             [runbld.opts :as opts]
+            [runbld.util.io :as io]
             [runbld.vcs.git :as git]
             :reload-all))
 
@@ -18,11 +21,37 @@
            (-> opts :email :to)))))
 
 (deftest check-out-repo
-  (let [opts1 (opts/parse-args ["-c" "test/runbld.yaml"
-                                "--job-name" "elastic,proj1,master"
-                                "/path/to/script.bash"])
-        _    (clojure.pprint/pprint opts1)
+  (let [;; clear clone cache
+        _ (io/rmdir-rf "tmp/git/cache")
+
+        ;; create fake upstream repo (what would be on github
+        ;; normally)
+        d (.getCanonicalPath
+           (jio/file
+            (format "tmp/git/cache/test-%s" (str (clj-time.core/now)))))
+        upstream (git/init-test-repo d)
+
+        ;; create opts with the fake github in place
+        opts1 (update-in
+               (opts/parse-args ["-c" "test/runbld.yaml"
+                                 "--job-name" "elastic,proj1,master"
+                                 "/path/to/script.bash"])
+               [:profiles :elastic-proj1-master :git]
+               assoc :remote d)
+
+        ;; now clone upstream to the cache tmp/git/cache/elastic/proj1
         opts2 ((build/wrap-build-meta
                 (build/wrap-merge-profile
-                 (build/wrap-git-repo
-                  identity))) opts1)]))
+                 identity)) opts1)
+
+        ;; clear out the workspace
+        _ (io/rmdir-rf (get-in opts2 [:build :workspace]))
+
+        ;; finally similate a build, where it does a fetch in the
+        ;; cached clone and then clones to the workspace
+        opts3 ((build/wrap-git-repo identity) opts2)]
+    ;; (clojure.pprint/pprint {:opts3 opts2})
+    (is (= 1 (count
+              (jgit/git-log
+               (jgit/load-repo
+                (get-in opts3 [:build :workspace]))))))))
