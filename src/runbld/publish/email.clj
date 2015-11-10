0a25cfa486f85e25b2cbd9f5ff9bbc568ce77396
Drew Raines <drew@raines.me>
2015-11-10 17:08:46 -0600

    Move ctx schema to its own var

--- src/runbld/publish/email.clj
+++ src/runbld/publish/email.clj
@@ -6,6 +6,45 @@
             [schema.core :as s]
             [stencil.core :as mustache]))
 
+(def Ctx
+  {
+   (s/required-key :commit) s/Str
+   (s/required-key :commit-time)  s/Str
+   (s/required-key :env) {s/Str s/Any}
+   (s/required-key :github-name) s/Str
+   (s/required-key :id) s/Str
+   (s/required-key :job-name) s/Str
+   (s/required-key :mail-from) s/Str
+   (s/required-key :org) s/Str
+   (s/required-key :project) s/Str
+   (s/required-key :rcpt-to) (s/cond-pre s/Str [s/Str])
+   (s/required-key :scriptfile) s/Str
+   (s/required-key :start-millis) s/Num
+   (s/required-key :time-end) s/Str
+   (s/required-key :time-start) s/Str
+
+   (s/optional-key :args) [s/Str]
+   (s/optional-key :author-email) s/Str
+   (s/optional-key :author-name) s/Str
+   (s/optional-key :branch) s/Str
+   (s/optional-key :cmd) [s/Str]
+   (s/optional-key :cmd-source) s/Str
+   (s/optional-key :cwd) s/Str
+   (s/optional-key :end-millis) s/Num
+   (s/optional-key :github-page) s/Str
+   (s/optional-key :jenkins-executor) (s/maybe s/Str)
+   (s/optional-key :jenkins-labels) (s/maybe s/Str)
+   (s/optional-key :jenkins-node) (s/maybe s/Str)
+   (s/optional-key :jenkins-number) (s/maybe s/Str)
+   (s/optional-key :job-name-extra) s/Str
+   (s/optional-key :profile-name) s/Str
+   (s/optional-key :program) s/Str
+   (s/optional-key :status) s/Num
+   (s/optional-key :took) s/Num
+   (s/optional-key :url) (s/maybe s/Str)
+   (s/optional-key :workspace) s/Str
+   })
+
 (defn maybe-split-addr [s]
   (if (and (string? s) (.contains s ","))
     (->> (str/split s #",")
@@ -22,43 +61,7 @@
 
 (s/defn send :- clojure.lang.IPersistentVector
   [opts :- Opts
-   ctx :- {
-           (s/required-key :commit) s/Str
-           (s/required-key :commit-time)  s/Str
-           (s/required-key :env) {s/Str s/Any}
-           (s/required-key :github-name) s/Str
-           (s/required-key :id) s/Str
-           (s/required-key :job-name) s/Str
-           (s/required-key :mail-from) s/Str
-           (s/required-key :org) s/Str
-           (s/required-key :project) s/Str
-           (s/required-key :rcpt-to) (s/cond-pre s/Str [s/Str])
-           (s/required-key :scriptfile) s/Str
-           (s/required-key :start-millis) s/Num
-           (s/required-key :time-end) s/Str
-           (s/required-key :time-start) s/Str
-
-           (s/optional-key :args) [s/Str]
-           (s/optional-key :author-email) s/Str
-           (s/optional-key :author-name) s/Str
-           (s/optional-key :branch) s/Str
-           (s/optional-key :cmd) [s/Str]
-           (s/optional-key :cmd-source) s/Str
-           (s/optional-key :cwd) s/Str
-           (s/optional-key :end-millis) s/Num
-           (s/optional-key :github-page) s/Str
-           (s/optional-key :jenkins-executor) (s/maybe s/Str)
-           (s/optional-key :jenkins-labels) (s/maybe s/Str)
-           (s/optional-key :jenkins-node) (s/maybe s/Str)
-           (s/optional-key :jenkins-number) (s/maybe s/Str)
-           (s/optional-key :job-name-extra) s/Str
-           (s/optional-key :profile-name) s/Str
-           (s/optional-key :program) s/Str
-           (s/optional-key :status) s/Num
-           (s/optional-key :took) s/Num
-           (s/optional-key :url) (s/maybe s/Str)
-           (s/optional-key :workspace) s/Str
-           }]
+   ctx :- Ctx]
   (send* (opts :email)
          (ctx :mail-from)
          (ctx :rcpt-to)
