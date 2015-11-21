d393b5ac760a59729451bb2ffa8265ad94630d77
Drew Raines <drew@raines.me>
2015-11-20 22:37:32 -0600

    Fix testcase collection?

--- src/runbld/post/test/junit.clj
+++ src/runbld/post/test/junit.clj
@@ -17,30 +17,44 @@
 (defn content-text [xml]
   (apply str (map x/text xml)))
 
+(defn collect-errors-and-failures [class test xml]
+  (merge
+   {:error-type (:tag xml)
+    :class class
+    :test test
+    :stacktrace (content-text (:content xml))}
+   (:attrs xml)))
+
 (defn failed-testcase [xml]
-  (merge (:attrs xml)
-         {:stacktrace (content-text (:content xml))}))
+  (let [{:keys [classname name]} (:attrs xml)]
+    (map (partial collect-errors-and-failures classname name)
+         (x/select xml [#{:failure :error :skipped}]))))
 
 (defn failed-testcases [xml]
-  (map failed-testcase
-       (x/select xml [[:testcase (x/has [:failure])]])))
+  (mapcat failed-testcase
+          (x/select xml [:testcase])))
 
 (defn make-failure-report [xml]
   (let [meta (select-keys
               (testcase-meta xml)
               [:name :errors :failures :tests :skipped])]
-    (when (pos? (:failures meta))
+    (when (or (pos? (:failures meta))
+              (pos? (:errors meta)))
       (assoc meta
              :testcases
              (failed-testcases xml)))))
 
 (defn combine-failure-reports [total testsuite]
-  (-> total
-      (update :errors + (:errors testsuite))
-      (update :failures + (:failures testsuite))
-      (update :tests + (:tests testsuite))
-      (update :skipped + (:skipped testsuite))
-      (update :testcases concat (:testcases testsuite))))
+  (try
+    (-> total
+        (update :errors + (:errors testsuite))
+        (update :failures + (:failures testsuite))
+        (update :tests + (:tests testsuite))
+        (update :skipped + (:skipped testsuite))
+        (update :testcases concat (:testcases testsuite)))
+    (catch Throwable t
+      (clojure.pprint/pprint testsuite)
+      (throw t))))
 
 (defn find-failures [dir]
   (when dir
@@ -53,7 +67,8 @@
          (map #(str "file://" %))
          (map #(java.net.URL. %))
          (map x/xml-resource)
-         (map make-failure-report)
+         (pmap make-failure-report)
+         (filter identity)
          (reduce combine-failure-reports {:errors 0
                                           :failures 0
                                           :tests 0
