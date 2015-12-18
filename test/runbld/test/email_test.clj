22b3be9987ddcf794adef734d2e1c6d27827b6d2
Drew Raines <drew@raines.me>
2015-12-18 12:04:03 -0600

    Fix email test

--- test/runbld/test/email_test.clj
+++ test/runbld/test/email_test.clj
@@ -12,4 +12,4 @@
             (io/resource "templates/email.mustache.html"))
            (edn/read-string
             (slurp "test/context.edn"))))
-    (is (= 1740 (count (slurp f))))))
+    (is (= 1552 (count (slurp f))))))
