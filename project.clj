4dc6f4d2202659760d97ff595f866e3d6b52e53c
Drew Raines <drew@raines.me>
2016-06-15 11:26:44 -0500

    elastic/lein-bin 0.3.6
    Fixes #31

--- project.clj
+++ project.clj
@@ -46,7 +46,7 @@
                  [stencil "0.5.0"]]
   :injections [(require 'clojure.pprint)]
   :profiles {:dev {:env {:dev true}}
-             :package {:plugins [[lein-bin "0.3.4"]]
+             :package {:plugins [[elastic/lein-bin "0.3.6"]]
                        :bin {:bootclasspath false}}}
   :plugins [[lein-environ "1.0.3"]]
   :aliases {"package" ["with-profile" "package" "bin"]}
