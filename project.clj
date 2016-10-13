6eb9030f6618d9711ac8c5701d892d17442fbe08
Drew Raines <drew@raines.me>
2016-10-13 16:28:46 -0500

    Add spec dependencies for 1.8.0

--- project.clj
+++ project.clj
@@ -37,6 +37,8 @@
                  [environ "1.0.3"]
                  [listora/again "0.1.0"]
                  [org.clojure/clojure "1.8.0"]
+                 [clojure-future-spec "1.9.0-alpha13"]
+                 [org.clojure/test.check "0.9.0"]
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/tools.cli "0.3.3"]
 
