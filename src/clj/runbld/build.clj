08f116affa2753540aa186f3ea56eaab70055c69
Mike Baamonde <mike.baamonde@elastic.co>
2017-03-29 14:16:46 -0400

    wip

--- src/clj/runbld/build.clj
+++ src/clj/runbld/build.clj
@@ -51,21 +51,32 @@
                                               project
                                               branch
                                               "multijob-intake"]))
-        query {:query
-               {:bool
-                {:must
-                 [{:term {:build.job-name.keyword intake-job}}
-                  {:term {:process.status.keyword "SUCCESS"}}]}},
-               :sort {:process.time-end {:order "desc"}},
-               :size 1}
-        res (doc/search es-conn idx {:body query})]
-    (-> res
-        :hits
-        :hits
-        first
-        :_source
-        :vcs
-        :commit-id)))
+        ;; post-5.0 mapping for string fields
+        q1 {:query
+            {:bool
+             {:filter
+              [{:term {:build.job-name.keyword intake-job}}
+               {:term {:process.status.keyword "SUCCESS"}}]}},
+            :sort {:process.time-end {:order "desc"}},
+            :size 1}
+        ;; pre-5.0 mapping for string fields
+        q2 {:query
+            {:bool
+             {:filter
+              [{:term {:build.job-name intake-job}}
+               {:term {:process.status "SUCCESS"}}]}},
+            :sort {:process.time-end {:order "desc"}},
+            :size 1}
+        commit (fn [doc]
+                 (-> doc
+                     :hits
+                     :hits
+                     first
+                     :_source
+                     :vcs
+                     :commit-id))]
+    (or (commit (doc/search es-conn idx {:body q1}))
+        (commit (doc/search es-conn idx {:body q2})))))
 
 (s/defn wrap-build-meta :- OptsWithBuild
   [proc :- clojure.lang.IFn]
