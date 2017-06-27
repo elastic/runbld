0b766b61ce49e20307c825e9a135cf4d3a015d15
David Leatherman <david.leatherman@elastic.co>
2017-06-27 09:39:40 -0400

    Add new debug log
    This will be emailed at the end of the test run if there was an exception.

---
+++ src/clj/runbld/util/email.clj
@@ -0,0 +1,9 @@
+(ns runbld.util.email
+  (:require [clojure.string :as str]
+            [schema.core :as s]))
+
+(s/defn obfuscate-addr :- s/Str
+  [addr :- s/Str]
+  (str/replace addr
+               (re-pattern "(.*?)@([^.]+\\.)?(.)[^.]+\\.([^.]+)$")
+               "$1@$3***.$4"))
