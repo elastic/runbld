3e71b349eb08d9da2b9715da4ecbbe515255ff86
Ron Toland <ron.toland@elastic.co>
2017-12-08 14:22:55 -0800

    order of metadata should not matter

--- test/check-metadata.bash
+++ test/check-metadata.bash
@@ -1,7 +1,8 @@
 exec > /dev/null 2>&1
 
-if [ "$BUILD_METADATA" = "the first metadata;the second metadata" ]; then
-    exit 0;
+if [[ "$BUILD_METADATA" =~ ^.*the.first.metadata.*$ &&
+      "$BUILD_METADATA" =~ ^.*the.second.metadata.*$ ]]; then
+    exit 0
 else
     exit 1
 fi
